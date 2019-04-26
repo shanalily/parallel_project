fc0259ca65c05093314c2fe2a684403605e0f8fb
shanalily <shanalily@gmail.com>
2019-04-26 14:40:40 -0400

    miles

--- sim_interstate.c
+++ sim_interstate.c
@@ -3,7 +3,7 @@
 #include<string.h>
 #include<errno.h>
 #include<math.h>
-#include <stddef.h>
+#include<stddef.h>
 
 #include "clcg4.h"
 #include<mpi.h>
@@ -43,30 +43,97 @@ MPI_Status status[4];
 
 unsigned int miles_per_rank;
 unsigned int global_row;
-unsigned int global_total_cars;
 unsigned int *interstate_west; // index 0 on eastern side, index miles_per_rank-1 on western side
 unsigned int *interstate_east; // index 0 on western side, index miles_per_rank-1 on western side
 unsigned int global_reached[NUM_TICKS]; // number of cars that have reached their destination.
 unsigned int local_reached[NUM_TICKS];
+unsigned int global_total_cars;
+unsigned int local_miles_travelled;
+unsigned int global_miles_travelled;
 
 /***************************************************************************/
 /* Functions ***********************************************************/
 /***************************************************************************/
 
+unsigned int mk_cars(float proportion);
+int update_roads(unsigned int miles_per_rank, unsigned int *interstate_west,
+        unsigned int *interstate_east, int can_move_west, int can_move_east);
+int reached_dest(unsigned int *interstate, int i, int east);
+int run_simulation(unsigned int *interstate_west, unsigned int *interstate_east);
+
+void print_interstate(unsigned int *interstate_west, unsigned int *interstate_east);
+
+
+
+int main(int argc, char *argv[]) {
+    // Example MPI startup and using CLCG4 RNG
+    MPI_Init( &argc, &argv);
+    MPI_Comm_size( MPI_COMM_WORLD, &mpi_commsize);
+    MPI_Comm_rank( MPI_COMM_WORLD, &mpi_myrank);
+    miles_per_rank = SIZE/mpi_commsize;
+    global_row = CAR*mpi_myrank*miles_per_rank;
+
+    InitDefault();
+
+    if (0 == mpi_myrank) g_start_cycles = GetTimeBase();
+
+    interstate_west = calloc(CAR*miles_per_rank, sizeof(int));
+    interstate_east = calloc(CAR*miles_per_rank, sizeof(int));
+
+    for (size_t i = 0; i < CAR*miles_per_rank; ++i) {
+        interstate_west[i] = 0;
+        interstate_east[i] = 0;
+    }
+
+    double proportion = 0.05;
+    int total_cars = mk_cars(proportion);
+    printf("total cars: %d\n", total_cars);
+    // print_interstate();
+    local_miles_travelled = run_simulation(interstate_west, interstate_east);
+
+    MPI_Reduce(&total_cars, &global_total_cars, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&local_miles_travelled, &global_miles_travelled, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(local_reached, global_reached, NUM_TICKS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (0 == mpi_myrank) {
+        g_end_cycles = GetTimeBase();
+        g_time_in_secs = ((double) (g_end_cycles - g_start_cycles)) /
+                    g_processor_frequency;
+    }
+
+    if (0 == mpi_myrank) {
+        for (size_t i = 0; i < NUM_TICKS; ++i) {
+            printf("global_state: %d\n", global_reached[i]);
+        }
+        printf("total cars: %d\n", global_total_cars);
+        printf("total miles travelled: %d\n", global_miles_travelled);
+        printf("Completed simulation of interstate traffic in %lf seconds.\n", g_time_in_secs);
+    }
+
+    free(interstate_west);
+    free(interstate_east);
+    MPI_Finalize();
+    return 0;
+}
+
+
+// Make cars at random locations on the highway. A car is marked by a 1 (as opposed to a 0)
+// at an even index, and the next entry contains the destination of the car.
 unsigned int mk_cars(float proportion){ 
     int row, total_cars = 0;
+    // do I double check here that the car is facing the correct direction?
     for (int i = 0; i < CAR*miles_per_rank; i += CAR) {
         row = i + mpi_myrank*miles_per_rank;
         interstate_west[i] = (int) (GenVal(row) < proportion);
         if (interstate_west[i]) {
-            interstate_west[i+1] = (int) (GenVal(row)*SIZE);
+            interstate_west[i+1] = (int) (GenVal(row)*SIZE); // should be in range i+2...2*miles_per_rank
             total_cars += 1;
         }
         else interstate_west[i+1] = 0;
 
         interstate_east[i] = (int) (GenVal(row+1) < proportion);
         if (interstate_east[i]) {
-            interstate_east[i+1] = (int) (GenVal(row+1)*SIZE);
+            interstate_east[i+1] = (int) (GenVal(row+1)*SIZE); // should be in range i+2...2*miles_per_rank
             total_cars += 1;
         }
         else interstate_east[i+1] = 0;
@@ -74,10 +141,10 @@ unsigned int mk_cars(float proportion){
     return total_cars;
 }
 
-
-// why is this not updating correctly?
-void update_roads(unsigned int miles_per_rank, unsigned int *interstate_west, unsigned int *interstate_east,
-        int can_move_west, int can_move_east) {
+// Update roads by moving cars to the east or west if there is space in front of them.
+int update_roads(unsigned int miles_per_rank, unsigned int *interstate_west,
+        unsigned int *interstate_east, int can_move_west, int can_move_east) {
+    unsigned int miles_travelled = 0;
     if (can_move_west) { // erase car from eastern side of road
         interstate_west[CAR*miles_per_rank-CAR] = 0;
         interstate_west[CAR*miles_per_rank-CAR+1] = 0;
@@ -91,18 +158,22 @@ void update_roads(unsigned int miles_per_rank, unsigned int *interstate_west, un
             interstate_west[i] = interstate_west[i-CAR];
             interstate_west[i+1] = interstate_west[i-CAR+1];
             interstate_west[i-CAR] = 0;
+            miles_travelled += 1;
         }
 
         if (!interstate_east[i]) {
             interstate_east[i] = interstate_east[i-CAR];
             interstate_east[i+1] = interstate_east[i-CAR+1];
             interstate_east[i-CAR] = 0;
+            miles_travelled += 1;
         }
     }
     interstate_west[0] = 0;
     interstate_west[1] = 0;
     interstate_east[0] = 0;
     interstate_east[1] = 0;
+
+    return miles_travelled;
 }
 
 int reached_dest(unsigned int *interstate, int i, int east) {
@@ -115,21 +186,9 @@ int reached_dest(unsigned int *interstate, int i, int east) {
     return 0;
 }
 
-void print_interstate() {
-    printf("Rank %d:\n", mpi_myrank);
-    for (int i = CAR*miles_per_rank-CAR; i >= 0; i -= CAR) {
-        printf("%d ", interstate_west[i]);
-    }
-    printf("\n");
-    for (int i = 0; i < CAR*miles_per_rank; i += CAR) {
-        printf("%d ", interstate_east[i]);
-    }
-    printf("\n");
-}
-
-void run_simulation(unsigned int *interstate_west, unsigned int *interstate_east) {
+int run_simulation(unsigned int *interstate_west, unsigned int *interstate_east) {
     int c1[1], c2[1], m1[1], m2[2];
-    int total_reached = 0, empty_val = -1, can_move_west=0, can_move_east=0;
+    int total_reached = 0, empty_val = -1, can_move_west=0, can_move_east=0, miles_travelled=0;
     int *empty = &empty_val;
     for (int i = 0; i < NUM_TICKS; ++i) {
         // one rank is sending a message to another rank to see if it can send a car. If sent, it will be set.
@@ -189,7 +248,7 @@ void run_simulation(unsigned int *interstate_west, unsigned int *interstate_east
         MPI_Barrier(MPI_COMM_WORLD);
 
         // update roads
-        update_roads(miles_per_rank, interstate_west, interstate_east, can_move_west, can_move_east);
+        miles_travelled += update_roads(miles_per_rank, interstate_west, interstate_east, can_move_west, can_move_east);
 
         // print_interstate();
 
@@ -215,54 +274,23 @@ void run_simulation(unsigned int *interstate_west, unsigned int *interstate_east
 
         printf("MPI_rank %d: total reached: %d\n", mpi_myrank, total_reached);
     }
+
+    return miles_travelled;
 }
 
-int main(int argc, char *argv[]) {
-    // Example MPI startup and using CLCG4 RNG
-    MPI_Init( &argc, &argv);
-    MPI_Comm_size( MPI_COMM_WORLD, &mpi_commsize);
-    MPI_Comm_rank( MPI_COMM_WORLD, &mpi_myrank);
-    miles_per_rank = SIZE/mpi_commsize;
-    global_row = CAR*mpi_myrank*miles_per_rank;
-
-    InitDefault();
-
-    if (0 == mpi_myrank) g_start_cycles = GetTimeBase();
-
-    interstate_west = calloc(CAR*miles_per_rank, sizeof(int));
-    interstate_east = calloc(CAR*miles_per_rank, sizeof(int));
-
-    for (size_t i = 0; i < CAR*miles_per_rank; ++i) {
-        interstate_west[i] = 0;
-        interstate_east[i] = 0;
+void print_interstate(unsigned int *interstate_west, unsigned int *interstate_east) {
+    printf("Rank %d:\n", mpi_myrank);
+    for (int i = CAR*miles_per_rank-CAR; i >= 0; i -= CAR) {
+        printf("%d ", interstate_west[i]);
     }
-
-    double proportion = 0.05;
-    int total_cars = mk_cars(proportion);
-    printf("total cars: %d\n", total_cars);
-    // print_interstate();
-    run_simulation(interstate_west, interstate_east);
-    
-
-    MPI_Reduce(&total_cars, &global_total_cars, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(local_reached, global_reached, NUM_TICKS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    if (0 == mpi_myrank) {
-        g_end_cycles = GetTimeBase();
-        g_time_in_secs = ((double) (g_end_cycles - g_start_cycles)) /
-                    g_processor_frequency;
+    printf("\n");
+    for (int i = 0; i < CAR*miles_per_rank; i += CAR) {
+        printf("%d ", interstate_east[i]);
     }
-
-    if (0 == mpi_myrank) {
-        for (size_t i = 0; i < NUM_TICKS; ++i) {
-            printf("global_state: %d\n", global_reached[i]);
-        }
-        printf("total cars: %d\n", global_total_cars);
-        printf("Completed simulation of interstate traffic in %lf seconds.\n", g_time_in_secs);
-    }
-
-    free(interstate_west);
-    free(interstate_east);
-    MPI_Finalize();
-    return 0;
+    printf("\n");
 }
+
+// make sure count is accurate
+// try to turn cars around if facing wrong direction
+// add distances the cars travel
+// add weather events
